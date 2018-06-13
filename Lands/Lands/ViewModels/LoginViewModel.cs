bea30dbb55d799ac36b7490008a90d8b585220a5
I.S.C. Javi Puc <angelpuc08@gmail.com>
2018-06-13 13:40:19 -0500

    Creamos las propiedades que vamos a usar en LoginViewModel e inicializamos el viewmodel de LoginViewModel en MainViewModel y pusimos los bindings de los controles del form, email,password, etc

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+  class LoginViewModel
+  {
+    #region Properties
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool IsRunning { get; set; }
+    public bool IsRemembered { get; set; }
+    public int MyProperty { get; set; }
+    #endregion
+
+    #region Constructors
+
+    public LoginViewModel()
+    {
+      this.IsRemembered = true;
+    }
+
+    #endregion
+
+    #region Commands
+    public ICommand LoginCommand { get; set; }
+    #endregion
+  }
+}
